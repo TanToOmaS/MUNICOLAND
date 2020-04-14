07e3d70ef5411489006373cc6abcb4e165d89464
TanToOmaS <alejandropplaza@gmail.com>
2020-04-14 19:47:04 +0200

    diseño home desde api primera version

--- home.php
+++ home.php
@@ -1,12 +1,9 @@
 <?php
-<<<<<<< HEAD
 
 include_once "user.php";
 include_once "user_session.php";
 include_once 'conexion.php';
 include_once "funciones_bd.php";
-=======
->>>>>>> master
 
 session_start();
 $username = $_SESSION['user'];
@@ -58,7 +55,6 @@ $asistenciasUsuario = array_column(mysqli_fetch_all(mysqli_query($conexion, $que
 </head>
 
 <body>
-<<<<<<< HEAD
     <!-- estructura dropdown para el navbar -->
     <ul id="dropdown1" class="dropdown-content">
         <li><a href="#!">LUCHA</a></li>
@@ -94,170 +90,54 @@ $asistenciasUsuario = array_column(mysqli_fetch_all(mysqli_query($conexion, $que
         </div> -->
         <div id='eventos'>
 
-            <?php include "ocurriendo.php";
-            //include "carousel.php"; 
-            ?>
-
-        </div>
-
-
-        <div class="row grey darken-3">
-            <h3 span class="tituloTorneo">EVENTOS DEL VIERNES 14 DE AGOSTO:</span>
-                <div class="col s8 m8 l8">
-                    <div class="carousel carousel-slider center">
-
-                        <?php
-
-                        while ($fila = mysqli_fetch_assoc($listaEventos14)) {
-                            $ID = $fila["ID"];
-                            $asiste = in_array($ID, $asistenciasUsuario);
-                        ?>
-
-                            <div class="carousel-fixed-item center">
-                                <a class="btn waves-effect teal lighten-2 white-text darken-text-2">ME PUNTO</a>
-                            </div>
-                            <div class="carousel-item white-text">
-                                <h2><?php echo  $fila['EVENTO'] ?></h2>
-                                <p class="white-text"><?php echo " EL " . $fila['FECHA'] . " EN " . $fila['LUGAR'] ?></p>
-                                <img src="<?php echo  $fila['IMAGEN'] ?>">
-                            </div>
-
-                        <?php } ?>
-                    </div>
-                </div>
-        </div>
-
-        <div class="row grey darken-3">
-            <h3 span class="tituloTorneo">EVENTOS DEL SÁBADO 15 DE AGOSTO:</span>
-                <div class="col s8 m8 l8">
-                    <div class="carousel carousel-slider center">
-
-                        <?php
-
-                        while ($fila = mysqli_fetch_assoc($listaEventos15)) {
-                            $ID = $fila["ID"];
-                            $asiste = in_array($ID, $asistenciasUsuario);
-                        ?>
-                            <div class="carousel-fixed-item center">
-                                <a class="btn waves-effect  teal lighten-2 white-text darken-text-2">ME PUNTO</a>
-                            </div>
-                            <div class="carousel-item white-text">
-                                <h2><?php echo  $fila['EVENTO'] ?></h2>
-                                <p class="white-text"><?php echo " EL " . $fila['FECHA'] . " EN " . $fila['LUGAR'] ?></p>
-                                <img src="<?php echo  $fila['IMAGEN'] ?>">
-                            </div>
-
-                        <?php } ?>
-
-                    </div>
-                </div>
-        </div>
-
-        <div class="row grey darken-3">
-            <h3 span class="tituloTorneo">EVENTOS DEL DOMINGO 16 DE AGOSTO:</span>
-                <div class="col s8 m8 l8">
-                    <div class="carousel carousel-slider center">
-                        <?php
-
-                        while ($fila = mysqli_fetch_assoc($listaEventos16)) {
-                            $ID = $fila["ID"];
-                            $asiste = in_array($ID, $asistenciasUsuario);
-                        ?>
-
-                            <div class="carousel-fixed-item center">
-                                <a class="btn waves-effect  teal lighten-2 white-text darken-text-2">ME PUNTO</a>
-                            </div>
-                            <div class="carousel-item white-text">
-                                <h2><?php echo  $fila['EVENTO'] ?></h2>
-                                <p class="white-text"><?php echo " EL " . $fila['FECHA'] . " EN " . $fila['LUGAR'] ?></p>
-                                <img src="<?php echo  $fila['IMAGEN'] ?>">
-                            </div>
-
-                        <?php }  ?>
-                    </div>
-                </div>
-        </div>
-
-</body>
-=======
-    <header>
-
-        <div class="menu_bar">
-            <a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="home.php"><span class="tiny material-icons">home</span>EL PUEBLO</a></li>
-                <li><a href="#"><span class="tiny material-icons">local_bar</span>FIESTAS</a></li>
-                <li><a href="torneos.php"><span class="tiny material-icons">videogame_asset</span>TORNEOS</a></li>
-                <li><a href="#"><span class="tiny material-icons">book</span>AZRAEL</a></li>
-                <li><a href="#"><span class="tiny material-icons">live_tv</span>MULTIMEDIA</a></li>
-                <li><a href="logout.php"><span class="tiny material-icons">exit_to_app</span>CERRAR SESIÓN</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <div id="home">
-        <div id="pueblo">
-            <h1 id="homehead">MUÑICOLAND</h1>
-        </div>
-        <!--<div id="tikitiki">
-            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTikitikisummertime%2F&tabs=timeline&width=340&height=700&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=489868141175282" width="340" height="700" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-    </div>-->
-
-        <div id='eventos'>
-
             <?php include "ocurriendo.php"; ?>
 
-        </div>
-        <div id="contenedor-eventos" class="row">
+        </div>      
 
-            <div id="plantilla-evento" class="col s2" style="display: none;">
-                <div class="card orange lighten-1">
-                    <div class="card-image">
-                        <img src="{{urlImagen}}">
-                        <span class="card-title">{{nombre}}</span>
-                        <button class="btn-floating btn-large pulse halfway-fab waves-effect waves-light green botonAsistir" 
-                            data-idevento="{{idEvento}}" 
-                            data-showonstart="{{asiste}}" 
-                            onclick="asistir('{{idEvento}}')">
-                            <i class="large material-icons">event_available</i>
-                        </button>
-                        <button class="btn-floating btn-large pulse halfway-fab waves-effect waves-light red botonNoAsistir" 
-                        data-idevento="{{idEvento}}" 
-                        data-showonstart="{{noAsiste}}" 
-                        onclick="noAsistir('{{idEvento}}')">
-                            <i class="large material-icons">event_busy</i>
-                        </button>
-                    </div>
-                    <div class="card-content">
-                        <p class="black" style="font-size: 18px">
-                            EL {{fecha}} EN {{lugar}}
-                        </p>
-                    </div>
+        <h3 span class="tituloTorneo">EVENTOS DEL DÍA:</span></h3>
+        <div id="contenedor-eventos" class="row grey darken-3">
+        <div id="plantilla-evento" class="col s3 m3 l3" style="display: none;">        
+            <div class="card orange lighten-1">
+                <div class="card-image">
+                    <img src="{{urlImagen}}">
+                    <span class="card-title">{{nombre}}</span>
+                    <button class="btn-floating btn-large pulse halfway-fab waves-effect waves-light green botonAsistir" data-idevento="{{idEvento}}" data-showonstart="{{asiste}}" onclick="asistir('{{idEvento}}')">
+                        <i class="large material-icons">event_available</i>
+                    </button>
+                    <button class="btn-floating btn-large pulse halfway-fab waves-effect waves-light red botonNoAsistir" data-idevento="{{idEvento}}" data-showonstart="{{noAsiste}}" onclick="noAsistir('{{idEvento}}')">
+                        <i class="large material-icons">event_busy</i>
+                    </button>
+                </div>
+                <div class="card-content">
+                    <p class="black" style="font-size: 18px">
+                        EL {{fecha}} EN {{lugar}}
+                    </p>
                 </div>
             </div>
-
         </div>
->>>>>>> master
-
-        <!-- ZONA JAVASCRIPT: -->
     </div>
-    <script type="text/javascript">
-        const username = '<?php echo $username; ?>';
-    </script>
-    <script type="text/javascript" src="assets/js/util.js"></script>
-    <script type="text/javascript" src="assets/js/eventos.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-<<<<<<< HEAD
+</body>
+
+
+    <!-- ZONA JAVASCRIPT: -->
+</div>
+<script type="text/javascript">
+    const username = '<?php echo $username; ?>';
+</script>
+<script type="text/javascript" src="assets/js/util.js"></script>
+<script type="text/javascript" src="assets/js/eventos.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 <script type="text/javascript" src="util.js"></script>
 <script type="text/javascript" src="botonesAsistir.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<!-- Script para carousel -->
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.carousel');
@@ -272,14 +152,10 @@ $asistenciasUsuario = array_column(mysqli_fetch_all(mysqli_query($conexion, $que
         $('.carousel').carousel('next');
     }, 4500);
     $(document).ready(function() {
-        $(".dropdown-trigger").dropdown(
-            {hover: true}
-            );
+        $(".dropdown-trigger").dropdown({
+            hover: true
+        });
     });
 </script>
-=======
-
-</body>
->>>>>>> master
 
 </html>
--- ocurriendo.php
+++ ocurriendo.php
@@ -2,7 +2,7 @@
 include('conexion.php');
 require('fechaCastellano.php');
 date_default_timezone_set('Europe/Madrid');
-setlocale (LC_TIME, "es_ES.UTF-8");
+setlocale(LC_TIME, "es_ES.UTF-8");
 
 $hora = date("G:i");
 $fecha = date("d-M-y");
@@ -10,15 +10,27 @@ $fechaCastellano = fechaCastellano($fecha);
 
 $evento = "SELECT * FROM eventos WHERE FECHA < CURRENT_TIMESTAMP";
 //$lugar = "SELECT LUGAR FROM eventos WHERE HORA < CURRENT_TIMESTAMP";
-$eventoActual = mysqli_query($conexion,$evento);
-echo "<h3 id='ocurriendo'>Actualmente son las ". $hora ." del ". $fechaCastellano ." </h3>";
+$eventoActual = mysqli_query($conexion, $evento);
+echo "<h3 id='ocurriendo'>Actualmente son las " . $hora . " del " . $fechaCastellano . " </h3>";
 
-while ($result = mysqli_fetch_assoc($eventoActual))
+while ($result = mysqli_fetch_assoc($eventoActual)) {
 
-{
-    
- echo "<h3 id='ocurriendo'>No te pierdas ahora el evento <span style='color: white'>". $result['EVENTO'] . "</span> en <span style='color: white'>". $result['LUGAR'] ."</span>.  </h3>";
-
+    echo "<h3 id='ocurriendo'>No te pierdas ahora el evento <span style='color: white'>" . $result['EVENTO'] . "</span> en <span style='color: white'>" . $result['LUGAR'] . "</span>.  </h3>";
 }
 
 ?>
+<div class="row grey darken-3">
+    <h3 span class="tituloTorneo">PRÓXIMOS EVENTOS:</span></h3>
+    <div class="col s8 m8 l8">
+        <div class="carousel carousel-slider center">
+            <div class="carousel-fixed-item center">
+                <a class="btn waves-effect teal lighten-2 white-text darken-text-2">ME PUNTO</a>
+            </div>
+            <div class="carousel-item white-text">
+                <h2>{{nombre}}</h2>
+                <p class="white-text">EL {{fecha}} EN {{lugar}}</p>
+                <img src="{{urlImagen}}">
+            </div>
+        </div>
+    </div>
+</div>
