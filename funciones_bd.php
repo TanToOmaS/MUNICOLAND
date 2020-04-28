306da1ac734eead336ebc6019eb0cb6dc6186a09
TanToOmaS <alejandropplaza@gmail.com>
2020-04-28 20:50:05 +0200

    controlando si un usuario ya existe

--- funciones_bd.php
+++ funciones_bd.php
@@ -4,15 +4,24 @@
 
 function crearUsuario($USUARIO,$NOMBRE,$APELLIDO,$EMAIL,$PASSWORD) {
 
-include "conexion.php";
+include_once "conexion.php";
+$emailsUsuarios = mysqli_query($conexion,"SELECT * FROM usuarios WHERE EMAIL = '$EMAIL'");
+
 $sentencia = "INSERT INTO usuarios VALUES (' ', '$USUARIO','$NOMBRE','$APELLIDO','$EMAIL','$PASSWORD')";
 
-if (mysqli_query($conexion,$sentencia)){
-	echo "<h3 style='color: white'>Se ha creado tu usuario correctamente</h3>";
-	echo "<form action='logout.php'><button type='submit'>INICIAR SESIÓN</button>";	
-}else{
-	echo "<h3 style='color: white'>Se ha producido un error en la inserción. Comprueba el error.</h3>";
-}
+	if 	(mysqli_num_rows($emailsUsuarios) > 0){
+		echo "alert('El usuario ya existe');";
+	
+	}else if (mysqli_query($conexion,$sentencia)){
+			echo "alert('Se ha creado tu usuario correctamente');";
+			echo "<form action='logout.php'><button type='submit'>INICIAR SESIÓN</button>";
+	}
+	else{
+		echo "<h3 style='color: white'>Se ha producido un error en la inserción. Comprueba el error.</h3>";
+	}
+	
+
+	
 mysqli_close($conexion);
 
 }
