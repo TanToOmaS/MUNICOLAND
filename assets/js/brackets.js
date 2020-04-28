59a43a95069c377376ccfb40f0c45ffebff350b3
TanToOmaS <alejandropplaza@gmail.com>
2020-04-28 19:40:12 +0200

    Agregar nombres de ronda

--- assets/js/brackets.js
+++ assets/js/brackets.js
@@ -60,8 +60,8 @@ function mostrarRondas(rondas) {
     });
     plantillaEnfrentamiento.remove();
     const plantillaRondaString = plantillaRonda.prop('outerHTML');    // Convertimos el elemento html a string
-    const plantillaRondaRellenada = plantillaRondaString
-      .replace(/{{nombreRonda}}/g, ronda.id);
+    const nombreRonda = nombresRonda.find(n => n.id === ronda.id).nombre;
+    const plantillaRondaRellenada = plantillaRondaString.replace(/{{nombreRonda}}/g, nombreRonda);
     const rondaHtml = $(plantillaRondaRellenada);
     rondaHtml.show();   // Anular display none de la plantilla
     rondaHtml.removeAttr('id');
@@ -78,52 +78,3 @@ const nombresRonda = [
   { id: 3, nombre: 'CUARTOS' },
   { id: 4, nombre: 'OCTAVOS' }
 ];
-
-let titles = [];
-$.each(rondasOrdenadas, function (i, ronda) {
-  const nombre = nombresRonda.find(n => n.id === ronda.id);
-  if (nombre) {
-    titles.push(nombre.nombre);
-  }
-});
-
-// $.each(enfrentamientos, function (i, enfrentamiento) {
-
-//   let eq1Gana = puntEquipo1 > puntEquipo2;
-//   let eq2Gana = puntEquipo2 > puntEquipo1;
-//   
-//   };
-// });
-
-// const rondaFinal = rondas.find(r => r.id === 1);
-// const enfrentamientoFinal = rondaFinal.enfrentamientos[0];
-// const puntEquipo1 = obtenerResultadoEnfrentamiento(enfrentamientoFinal, 1);
-// const puntEquipo2 = obtenerResultadoEnfrentamiento(enfrentamientoFinal, 2);
-// const eqGanador = puntEquipo1 > puntEquipo2 ? enfrentamientoFinal.equipo1 : enfrentamientoFinal.equipo2;
-// brackets.push(rondaCampeon);
-
-
-
-// $.each(rondasOrdenadas, (i, r) => rounds2[r.id] = []);
-
-// $.each(rondasOrdenadas, function (i, ronda) {
-//   var bracketsRonda = [];
-//   $.each(ronda.enfrentamientos, function (i, enfrentamiento) {
-//     const puntEquipo1 = obtenerResultadoEnfrentamiento(enfrentamiento, 1);
-//     const puntEquipo2 = obtenerResultadoEnfrentamiento(enfrentamiento, 2);
-//     let eq1Gana = puntEquipo1 > puntEquipo2;
-//     let eq2Gana = puntEquipo2 > puntEquipo1;
-//     const tituloEq1 = enfrentamiento.equipo1.nombre + '   |   ' + puntEquipo1;
-//     const tituloEq2 = enfrentamiento.equipo2.nombre + '   |   ' + puntEquipo2;
-//     const bracket = {
-//       player1: { name: tituloEq1, winner: eq1Gana, ID: enfrentamiento.equipo1.id },
-//       player2: { name: tituloEq2, winner: eq2Gana, ID: enfrentamiento.equipo2.id }
-//     };
-//     bracketsRonda.push(bracket);
-//   });
-//   brackets.push(bracketsRonda);
-// });
-
-
-
-
