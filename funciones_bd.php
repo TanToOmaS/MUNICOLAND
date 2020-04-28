db931c37d5250736928345d7852e46e034a7449e
TanToOmaS <alejandropplaza@gmail.com>
2020-04-28 21:12:08 +0200

    controlado si email está en uso

--- funciones_bd.php
+++ funciones_bd.php
@@ -10,13 +10,15 @@ $emailsUsuarios = mysqli_query($conexion,"SELECT * FROM usuarios WHERE EMAIL = '
 $sentencia = "INSERT INTO usuarios VALUES (' ', '$USUARIO','$NOMBRE','$APELLIDO','$EMAIL','$PASSWORD')";
 
 	if 	(mysqli_num_rows($emailsUsuarios) > 0){
-		echo "alert('El usuario ya existe');";
+		echo "<h3 style='color: white'>EL EMAIL YA ESTÁ REGISTRADO. POR FAVOR, UTILIZA OTRO.</h3>";
+		echo '<script type="text/javascript">alert("EL EMAIL YA ESTÁ EN USO");</script>';
 	
 	}else if (mysqli_query($conexion,$sentencia)){
 			echo "alert('Se ha creado tu usuario correctamente');";
 			echo "<form action='logout.php'><button type='submit'>INICIAR SESIÓN</button>";
 	}
 	else{
+		echo "alert('El usuario ya existe');";
 		echo "<h3 style='color: white'>Se ha producido un error en la inserción. Comprueba el error.</h3>";
 	}
 	
