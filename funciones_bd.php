c3fa32c6fa2e1062141de806afe34e4d3d798655
TanToOmaS <alejandropplaza@gmail.com>
2020-04-29 12:46:30 +0200

    control de errores - usuario o email ya existen

--- funciones_bd.php
+++ funciones_bd.php
@@ -2,30 +2,26 @@
 
 // CREO LAS FUNCIONES PARA EL FORMULARIO DE USUARIOS
 
-function crearUsuario($USUARIO,$NOMBRE,$APELLIDO,$EMAIL,$PASSWORD) {
+function crearUsuario($USUARIO, $NOMBRE, $APELLIDO, $EMAIL, $PASSWORD)
+{
 
-include_once "conexion.php";
-$emailsUsuarios = mysqli_query($conexion,"SELECT * FROM usuarios WHERE EMAIL = '$EMAIL'");
+	include_once "conexion.php";
+	$emailsUsuarios = mysqli_query($conexion, "SELECT * FROM usuarios WHERE EMAIL = '$EMAIL'");
+	$usuarioExiste = mysqli_query($conexion, "SELECT * FROM usuarios WHERE USUARIO = '$USUARIO'");
 
-$sentencia = "INSERT INTO usuarios VALUES (' ', '$USUARIO','$NOMBRE','$APELLIDO','$EMAIL','$PASSWORD')";
+	$sentencia = "INSERT INTO usuarios VALUES (' ', '$USUARIO','$NOMBRE','$APELLIDO','$EMAIL','$PASSWORD')";
 
-	if 	(mysqli_num_rows($emailsUsuarios) > 0){
-		echo "<h3 style='color: white'>EL EMAIL YA ESTÁ REGISTRADO. POR FAVOR, UTILIZA OTRO.</h3>";
-		echo '<script type="text/javascript">alert("EL EMAIL YA ESTÁ EN USO");</script>';
-	
-	}else if (mysqli_query($conexion,$sentencia)){
-			echo "alert('Se ha creado tu usuario correctamente');";
-			echo "<form action='logout.php'><button type='submit'>INICIAR SESIÓN</button>";
-	}
-	else{
-		echo "alert('El usuario ya existe');";
+	if (mysqli_num_rows($emailsUsuarios) > 0) {
+		echo '<script type="text/javascript">toast("warning", "!EL EMAIL YA ESTÁ EN USO!");</script>';
+	} else if (mysqli_num_rows($usuarioExiste) > 0) {
+		echo '<script type="text/javascript">toast("warning", "!EL USUARIO YA EXISTE!");</script>';
+	} else if (mysqli_query($conexion, $sentencia)) {
+		echo '<script type="text/javascript">toast("success", "!TE HAS REGISTRADO SATISFACTORIAMENTE!");</script>';				
+	} else {
 		echo "<h3 style='color: white'>Se ha producido un error en la inserción. Comprueba el error.</h3>";
 	}
-	
-
-	
-mysqli_close($conexion);
-
+	mysqli_close($conexion);
 }
-
 ?>
+<script type="text/javascript" src="assets/js/util.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
--- registro.php
+++ registro.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 // Motrar todos los errores de PHP
-error_reporting(-1); 
+error_reporting(-1);
 // Motrar todos los errores de PHP
 ini_set('error_reporting', E_ALL);
 //MANTENGO LA SESION ACTIVADA:
@@ -10,67 +10,70 @@ ini_set('error_reporting', E_ALL);
 ?>
 
 <!DOCTYPE html>
-<html lang = 'ES'>
+<html lang='ES'>
 <html>
-  <head>
+
+<head>
   <link rel="stylesheet" type="text/css" href="estilo.css">
-    <link rel="stylesheet" type="text/css" href="fonts.css">
-     <!-- Materialize icons -->
-     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- Materialize minified CSS -->
-    <link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
-    <style>
-        body {background-color: #000;}
-    </style>
-    <meta charset="utf-8">    
-    <title>REGÍSTRATE</title>
-  </head>
-  <body>
+  <link rel="stylesheet" type="text/css" href="fonts.css">
+  <!-- Materialize icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Materialize minified CSS -->
+  <link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
+  <style>
+    body {
+      background-color: #000;
+    }
+  </style>
+  <meta charset="utf-8">
+  <title>REGÍSTRATE</title>
+</head>
 
-<?php
+<body>
 
-include "funciones_bd.php";
+  <?php
 
-$USUARIO = $_POST["USUARIO"];
-$NOMBRE = $_POST["NOMBRE"];
-$APELLIDO = $_POST["APELLIDO"];
-$EMAIL = $_POST["EMAIL"];
-$PASSWORD = $_POST["PASSWORD"];
+  include "funciones_bd.php";
 
- ?>
+  $USUARIO = $_POST["USUARIO"];
+  $NOMBRE = $_POST["NOMBRE"];
+  $APELLIDO = $_POST["APELLIDO"];
+  $EMAIL = $_POST["EMAIL"];
+  $PASSWORD = $_POST["PASSWORD"];
 
-    <div id="home">
-        <h1 class="bienvenido">¡REGÍSTRATE PARA ENTRAR!</h1>    
-        <div id="escudo"><img class="boxshadow" width="200" height="133" hspace="20" src="index/banderaavila.png"> <img class="boxshadow" width="150" height="268" hspace="20" src="index/escudopueblo.jpg"><img class="boxshadow" width="200" height="133" hspace="20" src="index/banderaspain.png"></div>                
-        <hr style="color: #0056b2;"/>
-        <div class="logintitulo">CREA UNA CUENTA:
-        <p>O PINCHA <a href="index.php">AQUÍ</a> PARA INICIAR SESION</p>
-        </div>
-            <hr style="color: #0056b2;"/>
-            <br>
-                <form action="registro.php" method="post" class="formlogin">            
-                <input class="grey darken-4" type="text" placeholder="USUARIO" name="USUARIO"/>            
-                <br>                           
-                <input class="grey darken-4" type="text" placeholder="NOMBRE" name="NOMBRE"/>            
-                <br>                           
-                <input class="grey darken-4" type="text" placeholder="APELLIDO" name="APELLIDO"/>           
-                <br>                           
-                <input class="grey darken-4" type="email" placeholder="EMAIL" name="EMAIL"/>            
-                <br>                
-                <input class="grey darken-4" type="password" placeholder="PASSWORD" name="PASSWORD"/>
-                <br>                
-                <div class="button">
-                <button class="btn waves-effect waves-light" type="submit" name="CREAR">CREAR<i class="material-icons right">send</i></button>
-                </div>    
-                </form>
-        </div>
+  ?>
+
+  <div id="home">
+    <h1 class="bienvenido">¡REGÍSTRATE PARA ENTRAR!</h1>
+    <div id="escudo"><img class="boxshadow" width="200" height="133" hspace="20" src="index/banderaavila.png"> <img class="boxshadow" width="150" height="268" hspace="20" src="index/escudopueblo.jpg"><img class="boxshadow" width="200" height="133" hspace="20" src="index/banderaspain.png"></div>
+    <hr style="color: #0056b2;" />
+    <div class="logintitulo">CREA UNA CUENTA:
+      <p>O PINCHA <a href="index.php">AQUÍ</a> PARA INICIAR SESION</p>
     </div>
-<?php
-if (isset ($_POST ['CREAR'])){
-  crearUsuario($USUARIO,$NOMBRE,$APELLIDO,$EMAIL,$PASSWORD);
-}
-?>
-  </body>
+    <hr style="color: #0056b2;" />
+    <br>
+    <form action="registro.php" method="post" class="formlogin">
+      <input class="grey darken-4" type="text" placeholder="USUARIO" name="USUARIO" />
+      <br>
+      <input class="grey darken-4" type="text" placeholder="NOMBRE" name="NOMBRE" />
+      <br>
+      <input class="grey darken-4" type="text" placeholder="APELLIDO" name="APELLIDO" />
+      <br>
+      <input class="grey darken-4" type="email" placeholder="EMAIL" name="EMAIL" />
+      <br>
+      <input class="grey darken-4" type="password" placeholder="PASSWORD" name="PASSWORD" />
+      <br>
+      <div class="button">
+        <button class="btn waves-effect waves-light" type="submit" name="CREAR">CREAR<i class="material-icons right">send</i></button>
+      </div>
+    </form>
+  </div>
+  </div>
+  <?php
+  if (isset($_POST['CREAR'])) {
+    crearUsuario($USUARIO, $NOMBRE, $APELLIDO, $EMAIL, $PASSWORD);
+  }
+  ?>
+</body>
+
 </html>
-
-
